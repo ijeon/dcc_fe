c841eb0d63d682cd0de7ae5a82a79d12e24ab1ae
Inpyo Jeon <inpyojeon@hyundaihmall.com>
2023-05-25 11:17:29 +0900

    main

--- js/common.js
+++ js/common.js
@@ -2,3 +2,20 @@ const swiper = new Swiper('.swiper', {
 speed: 400,
 spaceBetween: 100,
 });
+
+$(document).on('click', '#btn-search-open', openSearchPane);
+$(document).on('focusout', '#search-input', closeSearchPane);
+
+function openSearchPane(){
+    $('#search-wrap').addClass('_opened');
+    setTimeout(function(){
+        $('#search-input').focus();
+    }, 100);
+}
+
+function closeSearchPane(){
+    $('#search-wrap').removeClass('_opened');
+    setTimeout(function(){
+        $('#search-input').val('');
+    }, 100);    
+}
